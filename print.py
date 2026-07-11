b91fba946575264ac108ebe05c42d621740c73e7
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-07-12 01:39:19 +0600

    feat(print.py): add basic print examples including text, numbers, expressions, f-string, escape characters, and custom separators

---
+++ print.py
@@ -0,0 +1,29 @@
+# Basic text print
+print("Hello, World!")
+
+# Print My name
+print("Islamul Hoque Ishfak")
+
+# Print numbers
+print(123)
+print(3.1416)
+
+# Print multiple values together
+print("My age is:", 22)
+
+# Print expressions
+print("Sum of 5 + 7 =", 5 + 7)
+
+# Using f-string for formatted output
+name = "Ishfak"
+goal = "CEO of Google"
+print(f"{name} will be {goal}, In-sha-allah")
+
+# Escape characters
+print("Line1\nLine2")   # newline
+print("Tab\tSpace")     # tab
+
+# Custom separator and end
+print("Python", "is", "fun", sep="-")
+print("Hello", end=" ")
+print("World")
