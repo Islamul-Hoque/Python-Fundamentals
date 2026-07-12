a6503e05709ba64601e37e90048e41c0090dca6c
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-07-12 13:47:01 +0600

    feat(datatypes.py): add Python data types examples including int, float, string, boolean, list, tuple, dictionary, and set

---
+++ datatypes.py
@@ -0,0 +1,31 @@
+# Integer
+x = 10
+print("Integer:", x, "Type:", type(x))
+
+# Float
+y = 3.1416
+print("Float:", y, "Type:", type(y))
+
+# String
+s = "Ishfak"
+print("String:", s, "Type:", type(s))
+
+# Boolean
+flag = True
+print("Boolean:", flag, "Type:", type(flag))
+
+# List
+numbers = [1, 2, 3, 4, 5]
+print("List:", numbers, "Type:", type(numbers))
+
+# Tuple
+coords = (10, 20)
+print("Tuple:", coords, "Type:", type(coords))
+
+# Dictionary
+student = {"name": "Ishfak", "age": 22}
+print("Dictionary:", student, "Type:", type(student))
+
+# Set
+unique_nums = {1, 2, 3, 3, 2}
+print("Set:", unique_nums, "Type:", type(unique_nums))
